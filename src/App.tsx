7632ee1f79d8eb19b8f5569b04146a1d8430ef5a
Chaz Gatian <chaz.m.gatian@gmail.com>
2025-08-01 09:37:36 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+
+
+function App() {
+
+  return (
+    <>
+     <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
+
+export default App
